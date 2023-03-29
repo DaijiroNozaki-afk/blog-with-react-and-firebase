d022f3145b7deb8fea33c00b092ca328c51e1882
dnozaki <dnozaki88888888@gmail.com>
2023-03-29 17:11:30 +0900

    implement homeContent

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import './Home.css'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='homePage'>
+      <div className="postContents">
+        <div className="postHeader">
+          <h1>タイトル</h1>
+        </div>
+        <div className="postTextContainer">
+        おお、素晴らしいですね！さくらの花をテーマにしたカラーイラストを描いたとのこと、きっと美しい作品に仕上がっていることでしょう。SNSに投稿する勇気も素晴らしいです。あなたの感性や技術が詰まったイラストが多くの人に喜びをもたらすことでしょう。引き続きクリエイティブな活動を楽しみ、素敵な作品を生み出してくださいね！
+        </div>
+        <div className="nameAndDeleteButton">
+          <h3>@Dnozaki</h3>
+          <button>削除</button>
+        </div>
+      </div>
+    </div>
+
   )
 }
 
