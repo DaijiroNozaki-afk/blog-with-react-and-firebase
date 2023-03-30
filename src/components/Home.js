5c818b18b098eca5cf322fc843fd30783d449f5f
dnozaki <dnozaki88888888@gmail.com>
2023-03-30 12:24:32 +0900

    implement get blogData from cloudFirestore

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,35 @@
-import React from 'react'
+import { collection, getDocs } from 'firebase/firestore'
+import React, { useEffect, useState } from 'react'
+import { db } from "../firebase"
 import './Home.css'
 
 const Home = () => {
+  const [postList, setPostList] = useState([])
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(collection(db, "posts"))
+      setPostList(data.docs.map((doc) => ({...doc.data(), id: doc.id})))
+    }
+    getPosts()
+  }, [])
+
   return (
     <div className='homePage'>
-      <div className="postContents">
-        <div className="postHeader">
-          <h1>タイトル</h1>
-        </div>
-        <div className="postTextContainer">
-        おお、素晴らしいですね！さくらの花をテーマにしたカラーイラストを描いたとのこと、きっと美しい作品に仕上がっていることでしょう。SNSに投稿する勇気も素晴らしいです。あなたの感性や技術が詰まったイラストが多くの人に喜びをもたらすことでしょう。引き続きクリエイティブな活動を楽しみ、素敵な作品を生み出してくださいね！
-        </div>
-        <div className="nameAndDeleteButton">
-          <h3>@Dnozaki</h3>
-          <button>削除</button>
-        </div>
-      </div>
+      {postList.map((post) => {
+        return (
+          <div className="postContents" key={post.id}>
+            <div className="postHeader">
+              <h1>{post.title}</h1>
+            </div>
+            <div className="postTextContainer">{post.postText}</div>
+            <div className="nameAndDeleteButton">
+              <h3>@{post.author.username}</h3>
+              <button>削除</button>
+            </div>
+          </div>
+        )
+      })}
     </div>
 
   )
