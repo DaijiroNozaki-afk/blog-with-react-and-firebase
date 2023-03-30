34f6ac629d792b2e841df28c6b0bf51780d15e3b
dnozaki <dnozaki88888888@gmail.com>
2023-03-30 14:34:28 +0900

    implement loginAuthOnly writing auth

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,16 +11,18 @@ const Navbar = ({isAuth}) => {
         <Link to="/">
             <FontAwesomeIcon icon={faHouse} />ホーム
         </Link>
-        <Link to="/createpost">
-            <FontAwesomeIcon icon={faFilePen} />記事投稿
-        </Link>
         {!isAuth ? (
             <Link to="/login">
                 <FontAwesomeIcon icon={faArrowRightToBracket} />ログイン
             </Link> ) : (
-            <Link to="/logout">
-                <FontAwesomeIcon icon={faArrowRightToBracket} />ログアウト
-            </Link> 
+                <>
+                    <Link to="/createpost">
+                        <FontAwesomeIcon icon={faFilePen} />記事投稿
+                    </Link>
+                    <Link to="/logout">
+                        <FontAwesomeIcon icon={faArrowRightToBracket} />ログアウト
+                    </Link> 
+                </>
         )}
     </nav>
   )
